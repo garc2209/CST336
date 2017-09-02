2a74c442696f6813f8ad04266e3e953595a41a19
Jose Garcia <jogarcia@csumb.edu>
2017-09-02 06:34:54 +0000

    adding files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+        <meta charset="utf-8">
+        
+    </head>
+    <body>
+
+    <h1>Welcome to Jose's CST 336 Website</h1>
+
+    </body>
+</html>
