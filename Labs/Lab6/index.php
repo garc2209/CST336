08374f06a01e905d3a91b381314ce23b74d1ffb7
Jose Garcia <jogarcia@csumb.edu>
2017-10-30 04:25:02 +0000

    lab 6

---
+++ Labs/Lab6/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+                      <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <title> Lab 6: Admin Login Page </title>
+    </head>
+    <body>
+        
+       <h1> Admin Login </h1>
+        
+        <form method="POST" action="loginProcess.php">
+            
+            Username: <input type="text" name="username"/> <br />
+            
+            Password: <input type="password" name="password"/> <br />
+            
+            <input type="submit" name="login" value="Login"/>
+            
+            <?php
+            
+            
+            if($_SESSION['invalid'] == true){
+                echo "Wrong Password or Username";
+                
+            }
+            ?>
+            
+            
+        </form>
+
+    </body>
+</html>
+
+<?php
+unset($_SESSION['invalid']);
+?>
